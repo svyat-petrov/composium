1847ec04ccfa6285b3fb8886602fde484d124739
svyatoslav.petrov <118916917+svyat-petrov@users.noreply.github.com>
2026-04-15 10:45:25 +0300

    add public API exports to __init__

---
+++ src/composium/core/__init__.py
@@ -0,0 +1,10 @@
+from .control import Control
+from .lazy_element import LazyElement
+from .query import Locator, Query
+
+__all__ = [
+    'Locator',
+    'Query',
+    'Control',
+    'LazyElement'
+]
