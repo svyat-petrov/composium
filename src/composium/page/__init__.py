e7553e0c0a3d29bcbe87bb06eba14498e652c5ee
svyatoslav.petrov <118916917+svyat-petrov@users.noreply.github.com>
2026-04-16 17:18:51 +0300

    add public api for BasePage, Item, Widget, Screen

---
+++ src/composium/page/__init__.py
@@ -0,0 +1,13 @@
+from .base_page import BasePage
+from .item import Item
+from .screen import Screen, ScreenMeta
+from .widget import Widget, WidgetMeta
+
+__all__ = [
+    "BasePage",
+    "Item",
+    "Widget",
+    "WidgetMeta",
+    "Screen",
+    "ScreenMeta",
+]
