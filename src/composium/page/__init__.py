e9cdbe059190e857f2b3278873be124d4a6d6128
svyatoslav.petrov <118916917+svyat-petrov@users.noreply.github.com>
2026-04-28 11:16:50 +0300

    add page objects public API

---
+++ src/composium/page/__init__.py
@@ -0,0 +1,14 @@
+from .base_page import BasePage
+from .item import Item, ItemMeta
+from .screen import Screen, ScreenMeta
+from .widget import Widget, WidgetMeta
+
+__all__ = [
+    'BasePage',
+    'Item',
+    'ItemMeta',
+    'Widget',
+    'WidgetMeta',
+    'Screen',
+    'ScreenMeta',
+]
